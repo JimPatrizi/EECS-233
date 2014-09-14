01a1044868cc46a35f1ea7e57b5e04dfa9f3a20e
Jim <jdp99@case.edu>
2014-09-14 12:33:15 -0400

    Constructors added

--- P1/src/NumArrayList.java
+++ P1/src/NumArrayList.java
@@ -1,4 +1,23 @@
 
 public class NumArrayList {
+	private double [] list = null; //list itself. 
+	private int capacity; //max capacity of the list
+	private int num = 0; //current size of the list
+	private static final int DEFAULT_CAPACITY = 100;
+	
 
+	public NumArrayList(){
+		this(DEFAULT_CAPACITY);
+		}
+	
+	public NumArrayList(int capacity){
+		this.capacity = capacity;
+		list = new double[capacity]; //this is null array
+		num = 0;
+	}
+	
+	public int size(){
+		return this.num;
+	}
+		
 }
