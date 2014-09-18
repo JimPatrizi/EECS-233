a09e0712eb1080f128db21f640bc774550ccbf29
Jim <jdp99@case.edu>
2014-09-18 18:19:37 -0400

    Required testing and methods for NumLinkedList are done and working and compiling.

--- P1/src/NumLinkedList.java
+++ P1/src/NumLinkedList.java
@@ -1,79 +1,102 @@
 /**
- * Represents an ordered sequence of double precision floating point numbers. LinkedList Implementation.
- * EECS 233 P1
+ * Represents an ordered sequence of double precision floating point numbers.
+ * LinkedList Implementation. EECS 233 P1
+ * 
  * @author James Patrizi
  *
  */
 public class NumLinkedList implements NumList {
-	
+
 	/**
-	 * Node class
+	 * Node class	
 	 */
-	private class LLNode{
-	
-		private double element; //Stores value of the node
-		private LLNode next;//a reference to the next node in the list.
-		
+	private class LLNode {
+
+		private double element; // Stores value of the node
+		private LLNode next;// a reference to the next node in the list.
+
 		/**
 		 * the constructor of the node.
-		 * @param element - element in node
-		 * @param next - reference to the next node in the list.
+		 * 
+		 * @param element
+		 *            - element in node
+		 * @param next
+		 *            - reference to the next node in the list.
 		 */
-		public LLNode(double element , LLNode next){
+		public LLNode(double element, LLNode next) {
 			this.element = element;
 			this.next = next;
 		}
-		
+
 		/**
 		 * Returns the element.
+		 * 
 		 * @return -element of the current node in the list.
 		 */
-		public double getElement(){
+		public double getElement() {
 			return this.element;
 		}
-		
+
 		/**
 		 * Returns the address of the next node.
+		 * 
 		 * @return - returns the address of the next node in the list.
 		 */
-		public LLNode next(){
+		public LLNode next() {
 			return this.next;
 		}
-		
-		
-		public void setNext(LLNode next){
+
+		public void setNext(LLNode next) {
 			this.next = next;
 		}
 	}
-	
-	private LLNode head;//holds the head of the linked list.
-	private LLNode tail;//holds the tail of the linked list.
-	private int size; //number of elements in the list.
-	
+
+	private LLNode head;// holds the head of the linked list.
+	private LLNode tail;// holds the tail of the linked list.
+	private int size; // number of elements in the list.
+
 	/**
 	 * Constructor that created an empty linked list.
 	 */
-	public NumLinkedList(){
+	public NumLinkedList() {
 		this.head = null;
 		this.tail = null;
 		this.size = 0;
 	}
-	
+
 	/**
 	 * @return - returns the address of the head of the list.
 	 */
-	public LLNode getHead(){
+	public LLNode getHead() {
 		return this.head;
 	}
-	
+
 	/**
 	 * 
 	 * @return - returns the address of the tail of the list.
 	 */
-	public LLNode getTail(){
+	public LLNode getTail() {
 		return this.tail;
 	}
-	
+
+	/**
+	 * 
+	 * @param node
+	 *            - the node that head is then set to.
+	 */
+	public void setHead(LLNode node) {
+		this.head = node;
+	}
+
+	/**
+	 * 
+	 * @param node
+	 *            - the node that tail is then set to.
+	 */
+	public void setTail(LLNode node) {
+		this.tail = node;
+	}
+
 	/**
 	 * @return - returns the size of the list.
 	 */
@@ -81,46 +104,70 @@ public class NumLinkedList implements NumList {
 		return this.size;
 	}
 
-	
 	public void add(double value) {
-		// TODO Auto-generated method stub
-		
+		if (size == 0) {
+			setHead(new LLNode(value, null));
+			setTail(getHead());
+			this.size = 1;
+		} else {
+			LLNode nextNode = new LLNode(value, null);
+			getTail().setNext(nextNode);
+			setTail(nextNode);
+			size++;
+		}
+
 	}
 
-	
 	public void insert(int i, double value) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
 	public void remove(int i) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
 	public boolean contains(double value) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	
 	public double lookup(int i) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	
 	public boolean equals(NumList otherList) {
-		// TODO Auto-generated method stub
-		return false;
+		return (this.toString().equals(otherList.toString()));
 	}
 
-	
 	public void removeDuplicates() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	public String toString() {
+		if (size == 0) {
+			return "";
+		} else {
+			StringBuilder result = new StringBuilder();
+			LLNode ndptr = getHead();
+			for (; ndptr.next() != getTail(); ndptr = ndptr.next()) {// will
+																		// loop
+																		// to
+																		// second
+																		// to
+																		// last
+																		// node.
+				result.append(ndptr.getElement());
+				result.append(" ");
+			}
+			result.append(ndptr.getElement());
+			result.append(" ");
+			ndptr = ndptr.next();
+			result.append(ndptr.getElement());
+			return result.toString();
+		}
+	}
 }
