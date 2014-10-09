a23d9a2820f617255b9790a345c9f60f7c9fee4c
Jim <jdp99@case.edu>
2014-10-08 20:03:00 -0400

    bytecounter skeleton done

--- P2/src/ByteCounter.java
+++ P2/src/ByteCounter.java
@@ -1,8 +1,41 @@
 /**
  * 
- * @author Jim Patrizi
+ * @author James Patrizi
+ * EECS 233 P2
  *
  */
 public class ByteCounter {
-
+	
+	public ByteCounter(byte[] b){
+		
+	}
+	
+	public ByteCounter(String file){
+		
+	}
+	
+	public int getCount(byte b){
+		return 0;
+	}
+	
+	public int[] getCount(byte[] b){
+		int[] barray = {0};
+		return barray;
+	}
+	
+	public byte[] getElements(){
+		return null;
+	}
+	
+	public void setOrder(String order){
+		
+	}
+	
+	public String toString(){
+		return "";
+	}
+	
+	public String toString(String format){
+		return "";
+	}
 }
