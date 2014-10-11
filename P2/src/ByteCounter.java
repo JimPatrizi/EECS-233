c1e4f9c50bc7e296771e00be72a3ecb4a1088445
Jim <jdp99@case.edu>
2014-10-10 20:06:53 -0400

    Fund bytecounter code done, changed getElements test slightly, think what was given was slightly wrong

--- P2/src/ByteCounter.java
+++ P2/src/ByteCounter.java
@@ -42,15 +42,17 @@ public class ByteCounter {
 	public int[] getCount(byte[] b){
 		int [] array = new int[b.length];
 		for(int i = 0; i < b.length ; i++){
-			//for( byte i : b){
 			array[i]= getCount(b[i]);
 		}
 		return array;
 	}
 		
-	
 	public byte[] getElements(){
-		return null;
+		byte [] array = new byte[byteList.size()];
+		for(int i = 0 ; i < byteList.size();i++){
+			array[i] = byteList.get(i);
+		}
+		return array;
 	}
 	
 	public void setOrder(String order){
@@ -58,10 +60,20 @@ public class ByteCounter {
 	}
 	
 	public String toString(){
-		return "";
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < byteList.size() - 1;i++ ){
+			sb.append(byteList.get(i).intValue());
+			sb.append(':');
+			sb.append(intList.get(i));
+			sb.append(" ");
+			}
+		sb.append(byteList.get(byteList.size()- 1));
+		sb.append(':');
+		sb.append(intList.get(byteList.size()-1));
+		return sb.toString();
 	}
 	
 	public String toString(String format){
-		return "";
+		return"";
 	}
 }
