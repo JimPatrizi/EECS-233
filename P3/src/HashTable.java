18e1f2db5423d15bbbc7212b3732e78144841381
Jim <jdp99@case.edu>
2014-11-08 15:35:02 -0500

    HashTable Finished. Fund testing complete.

--- P3/src/HashTable.java
+++ P3/src/HashTable.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  * 
  * @author James Patrizi
@@ -7,11 +10,20 @@
  */
 public class HashTable {
 	
+	private ArrayList<LinkedList<HashEntry>> hashtable = new ArrayList<LinkedList<HashEntry>>();
+	public int tablesize = 0;
+	public int numItems = 0;
+
+	
 	/**
 	 * 
 	 */
 	public HashTable(){
-		
+	    hashtable = new ArrayList<LinkedList<HashEntry>>(100);
+		this.tablesize = 100;
+		for(int i = 0; i < tablesize; i++){
+			hashtable.add(i,new LinkedList<HashEntry>());
+		}
 	}
 	
 	/**
@@ -19,7 +31,11 @@ public class HashTable {
 	 * @param size -size of hashtable user wants
 	 */
 	public HashTable(int size){
-		
+		hashtable = new ArrayList<LinkedList<HashEntry>>(size);
+		this.tablesize = size;
+		for(int i = 0; i < tablesize; i++){
+			hashtable.add(i,new LinkedList<HashEntry>());
+		}
 	}
 	
 	/**
@@ -28,8 +44,49 @@ public class HashTable {
 	 * @param value
 	 */
 	public void put(String key, int value){
-		
+		if(value == 0){
+			throw new NullPointerException();
+		}
+		if(computeLoadFactor() < 1.0){
+			HashEntry newEntry = new HashEntry(key,value);
+			int index = Math.abs(newEntry.getKey().hashCode()%tablesize);
+			hashtable.get(index).add(newEntry);
+		    numItems++;
+		}
+		else{
+			rehash();
+			HashEntry newEntry = new HashEntry(key,value);
+			int index = Math.abs(newEntry.getKey().hashCode()%tablesize);
+			hashtable.get(index).add(newEntry);
+		    numItems++;
+		}
 	}
+	
+	public void rehash(){
+		HashTable copy = new HashTable(tablesize * 2);
+		for(LinkedList<HashEntry> i : hashtable){
+			for(HashEntry element : i){
+				copy.put(element.getKey(),element.getValue());
+			}
+		}
+		tablesize = tablesize*2;
+		numItems = copy.getNumItems();
+		hashtable = copy.getHashTable();
+	}
+	
+	public int getNumItems(){
+		return this.numItems;
+	}
+	
+	public double computeLoadFactor(){
+		double loadfactor = (getNumItems()+ 1)/tablesize;
+		return loadfactor;
+	}
+	
+	public ArrayList<LinkedList<HashEntry>> getHashTable(){
+		return hashtable;
+	}
+	
 	
 	/**
 	 * 
@@ -38,7 +95,19 @@ public class HashTable {
 	 * @param hashCode
 	 */
 	public void put(String key, int value, int hashCode){
-		
+		if(computeLoadFactor() < 1.0){
+			HashEntry newEntry = new HashEntry(key,value);
+			int index = Math.abs(hashCode) % tablesize;
+			hashtable.get(index).add(newEntry);
+		    numItems++;
+		}
+		else{
+			rehash();
+			HashEntry newEntry = new HashEntry(key,value);
+			int index = Math.abs(hashCode) % tablesize;
+			hashtable.get(index).add(newEntry);
+		    numItems++;
+		}
 	}
 	
 	/**
@@ -47,7 +116,18 @@ public class HashTable {
 	 * @param value
 	 */
 	public void update(String key, int value){
-		
+		int index = Math.abs(key.hashCode()) % tablesize;
+		LinkedList<HashEntry> list = hashtable.get(index);
+		if(list.size()==0){
+			put(key,value);
+		}
+		else {
+			for(int i = 0; i < list.size(); i++){
+				if(list.get(i).getKey() == key){
+					list.get(i).setValue(value);
+				}
+			}
+		}
 	}
 	
 	/**
@@ -56,7 +136,39 @@ public class HashTable {
 	 * @return
 	 */
 	public int get(String key){
-		return 0;
+		int index = Math.abs(key.hashCode()) % tablesize;
+		LinkedList<HashEntry> list = hashtable.get(index);
+		if(list.size() == 0){
+			return -1;
+		}
+		
+		else{
+			for(int i = 0 ; i < list.size(); i++){
+				if(list.get(i).getKey() == key){
+					return list.get(i).getValue();
+				}
+			}
+		}
+		return -1;
 	}
-
+	
+	public int get(String key, int hashCode){
+		int index = Math.abs(hashCode) % tablesize;
+		LinkedList<HashEntry> list = hashtable.get(index);
+		if(list.size() == 0){
+			return -1;
+		}
+		
+		else{
+			for(int i = 0 ; i < list.size(); i++){
+				if(list.get(i).getKey() == key){
+					return list.get(i).getValue();
+				}
+			}
+		}
+		return -1;
+	}
 }
+		
+
+
